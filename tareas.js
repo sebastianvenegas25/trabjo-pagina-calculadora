e4e3f387d27d86a216e4430c53e94fd14ce18559
sebastianvenegas25 <svenegasv@inemkennedy.edu.co>
2023-03-08 20:25:37 -0500

    pagina de calculadora

---
+++ tareas.js
@@ -0,0 +1,39 @@
+var suma =function(){
+    var valorx=
+document.getElementById('valor1').value
+    var valory=
+document.getElementById('valor2').value
+    document.getElementById('resultado').innerHTML=
+"El resultado de la suma es: " + (parseInt(valorx)+
+parseInt(valory))
+}
+
+var resta =function(){
+    var valorx=
+document.getElementById('valor1').value
+    var valory=
+document.getElementById('valor2').value
+    document.getElementById('resultado').innerHTML=
+"El resultado de la resta es: " + (parseInt(valorx)-
+parseInt(valory))
+}
+
+var multiplicacion =function(){
+    var valorx=
+document.getElementById('valor1').value
+    var valory=
+document.getElementById('valor2').value
+    document.getElementById('resultado').innerHTML=
+"El resultado de la multiplicacion es: " + (parseInt(valorx)*
+parseInt(valory))
+}
+
+var division =function(){
+    var valorx=
+document.getElementById('valor1').value
+    var valory=
+document.getElementById('valor2').value
+    document.getElementById('resultado').innerHTML=
+"El resultado de la division es: " + (parseInt(valorx)/
+parseInt(valory))
+}
